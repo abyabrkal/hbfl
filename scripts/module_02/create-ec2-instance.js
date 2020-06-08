fb89352f84ad977628cc416933084df5f4aa1f3c
Aby Kal <abyabrkal@gmail.com>
2020-06-08 19:50:01 +0400

    Import AWS from sdk

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,40 +1,40 @@
 // Imports
-// TODO: Import the aws-sdk
-const helpers = require('./helpers')
+const AWS = require("aws-sdk");
+const helpers = require("./helpers");
 
 // TODO: Configure region
 
 // Declare local variables
 // TODO: Create an ec2 object
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+const sgName = "hamster_sg";
+const keyName = "hamster_key";
 
 // Do all the things together
 createSecurityGroup(sgName)
-.then(() => {
-  return createKeyPair(keyName)
-})
-.then(helpers.persistKeyPair)
-.then(() => {
-  return createInstance(sgName, keyName)
-})
-.then((data) => {
-  console.log('Created instance with:', data)
-})
-.catch((err) => {
-  console.error('Failed to create instance with:', err)
-})
+  .then(() => {
+    return createKeyPair(keyName);
+  })
+  .then(helpers.persistKeyPair)
+  .then(() => {
+    return createInstance(sgName, keyName);
+  })
+  .then((data) => {
+    console.log("Created instance with:", data);
+  })
+  .catch((err) => {
+    console.error("Failed to create instance with:", err);
+  });
 
 // Create functions
 
-function createSecurityGroup (sgName) {
+function createSecurityGroup(sgName) {
   // TODO: Implement sg creation & setting SSH rule
 }
 
-function createKeyPair (keyName) {
+function createKeyPair(keyName) {
   // TODO: Create keypair
 }
 
-function createInstance (sgName, keyName) {
+function createInstance(sgName, keyName) {
   // TODO: create ec2 instance
 }
